618dd5c1f4351f3b2e3c80a11dba355ced2300cc
Matt McCoy <mattmccoy@Matts-MBP.attlocal.net>
2020-05-04 10:53:17 -0500

    First commit

---
+++ scripts/requests.js
@@ -0,0 +1,13 @@
+const getPuzzle = (wordCount) => {
+    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error("Unable to fetch puzzle")
+        }
+    }).then((data) => {
+        return data.puzzle
+    })
+}
+
+
